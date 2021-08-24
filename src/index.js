52518caee4be37e2843e025515b92123fdd25fe5
Ska <denguezli.iskander@gmail.com>
2021-08-24 16:39:41 +0100

    Script file remove

--- src/index.js
+++ src/index.js
@@ -44,3 +44,38 @@ const targets = document.querySelectorAll(".show-on-scroll")
 targets.forEach(function (target) {
   appearOnScroll.observe(target)
 })
+
+
+/* -------------------------- Show Letter by letter ------------------------- */
+var textWrapperAll = document.querySelectorAll(".hero__text--title")
+
+for (index in textWrapperAll) {
+  const textWrapper = textWrapperAll[index]
+  textWrapper.innerHTML = textWrapper.textContent.replace(
+    /\S/g,
+    "<span class='letter'>$&</span>"
+  )
+
+  anime
+    .timeline({ loop: false })
+
+    .add({
+      targets: ".letter",
+      translateY: ["1.1em", 0],
+      translateX: ["0.55em", 0],
+      translateZ: 0,
+      rotateZ: [180, 0],
+      duration: 750,
+      easing: "easeOutExpo",
+      delay: (el, i) => 50 * i,
+    })
+}
+
+// .add({
+//   targets: ".hero__text--title",
+//   opacity: 0,
+//   duration: 1000,
+//   easing: "easeOutExpo",
+//   delay: 1000,
+// })
+
